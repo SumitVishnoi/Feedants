c61bd3f0cd6ae906cd3288b5b651e0530f91ebde
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-09-24 10:05:14 +0530

    feat: initialize backend with user authentication
    - added server.js to set up Express server and connect to MongoDB
    - created token generation and verification functions in generateToken.js
    - developed user authentication controller with register, login, logout, and getMe functionalities
    - added user middleware for token verification
    - set up authentication routes for user registration, login, logout, and fetching current user

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
